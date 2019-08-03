b642a08927129d86d2de2d5febaf57a49bda4c71
Gergely Muka <muka.gergely@gmail.com>
2019-08-03 12:36:25 +0200

    Request Header Parser route added

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+// Added By Gergely Muka - 20190803
+app.get("/api/whoami", function (req, res) {
+  res.send({
+    ipaddress: req.header("x-forwarded-for"),
+    language:  req.header("accept-language").split(",")[0],
+    software:  req.header("user-agent").match(/\(([^]+)\)/)[1],
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
